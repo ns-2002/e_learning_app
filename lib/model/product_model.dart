2167bb7486a7be061ff1a1e643bfbb862dfbe268
ns-2002 <look4nandinisingh@gmail.com>
2022-07-16 15:19:22 +0530

    first commit

---
+++ lib/model/product_model.dart
@@ -0,0 +1,48 @@
+// ignore_for_file: prefer_const_constructors, duplicate_import
+
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+class Product {
+  final String image, title;
+  final int id, courses;
+  final Color color;
+  Product({
+    required this.image,
+    required this.title,
+    required this.courses,
+    required this.color,
+    required this.id,
+  });
+}
+
+List<Product> products = [
+  Product(
+    id: 1,
+    title: "Graphic Design",
+    image: "assets/images/graphics.png",
+    color: Color(0xFF71b8ff),
+    courses: 16,
+  ),
+  Product(
+    id: 2,
+    title: "Programming",
+    image: "assets/images/programming.png",
+    color: Color(0xFFff6374),
+    courses: 22,
+  ),
+  Product(
+    id: 3,
+    title: "Finance",
+    image: "assets/images/finance.png",
+    color: Color(0xFFffaa5b),
+    courses: 15,
+  ),
+  Product(
+    id: 4,
+    title: "UI/Ux design",
+    image: "assets/images/ux.png",
+    color: Color(0xFF9ba0fc),
+    courses: 18,
+  ),
+];
